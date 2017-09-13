8d9a14a77c5b917de5b6c9e4f71262a99ceafd3d
Daniel Koza <dakolech@gmail.com>
2017-09-13 12:13:21 +0200

    test env variable

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -4,12 +4,13 @@ import './generated-font/css/merels.css';
 import '../node_modules/font-awesome/css/font-awesome.css';
 import { BoardContainerComponent } from './game/components/boardContainer/boardContainer.component';
 
-// const logo = require('./logo.svg');
-
 class App extends React.Component<{}, {}> {
   render() {
     return (
-      <BoardContainerComponent />
+      <div>
+        <BoardContainerComponent />
+        <footer> {process.env.REACT_APP_BRANCH} </footer>
+      </div>
     );
   }
 }
